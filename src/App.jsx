73f2bc9d709bd870f41d74385614a189c19dc12e
IgorFlp <igor.felipe.13@hotmail.com>
2025-05-01 11:59:31 -0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Converter from "./pages/Converter";
+import History from "./pages/History";
+import Favorites from "./pages/Favorites";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/converter" element={<Converter />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
